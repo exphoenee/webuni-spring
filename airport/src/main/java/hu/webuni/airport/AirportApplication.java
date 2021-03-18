691916ca87b20a180de0190d164b9c0c09b35481
exphoenee <61277296+exphoenee@users.noreply.github.com>
2021-03-18 16:11:47 +0100

    Prepared

--- airport/src/main/java/hu/webuni/airport/AirportApplication.java
+++ airport/src/main/java/hu/webuni/airport/AirportApplication.java
@@ -1,27 +1,25 @@
 package hu.webuni.airport;
 
+import hu.webuni.airport.service.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import hu.webuni.airport.service.PriceService;
-
 @SpringBootApplication
 public class AirportApplication implements CommandLineRunner {
 
-	@Autowired
-	private PriceService priceService;
-	
-	//git proba
-	public static void main(String[] args) {
-		SpringApplication.run(AirportApplication.class, args);
-	}
+  @Autowired
+  private PriceService priceService;
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(priceService.getFinalPrice(200));
-		System.out.println(priceService.getFinalPrice(20000));
-	}
+  //git proba
+  public static void main(String[] args) {
+    SpringApplication.run(AirportApplication.class, args);
+  }
 
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println(priceService.getFinalPrice(200));
+    System.out.println(priceService.getFinalPrice(20000));
+  }
 }
